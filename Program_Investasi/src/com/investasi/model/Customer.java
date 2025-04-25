a15eba3a6f30a703bed1c19686fedafe5cf1b4a6
Steven <akungaming1395@gmail.com>
2025-04-25 22:49:18 +0800

    membuat Portofolio, User dan Customer sebagai extend user

--- Program_Investasi/src/com/investasi/model/Customer.java
+++ Program_Investasi/src/com/investasi/model/Customer.java
@@ -1,5 +1,14 @@
 package com.investasi.model;
 
-public class Customer {
+public class Customer extends User {
+    private Portofolio portofolio;
 
+    public Customer(String username, String password) {
+        super(username, password);
+        this.portofolio = new Portofolio();
+    }
+
+    public Portofolio getPortofolio() {
+        return portofolio;
+    }
 }
--- Program_Investasi/src/com/investasi/model/Portofolio.java
+++ Program_Investasi/src/com/investasi/model/Portofolio.java
@@ -1,5 +1,38 @@
 package com.investasi.model;
 
-public class Portofolio {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Portofolio {
+    private Map<String, Integer> sahamDimiliki;
+    private Map<String, Double> sbnDimiliki;
+
+    public Portofolio() {
+        sahamDimiliki = new HashMap<>();
+        sbnDimiliki = new HashMap<>();
+    }
+
+    public Map<String, Integer> getSahamDimiliki() {
+        return sahamDimiliki;
+    }
+
+    public Map<String, Double> getSbnDimiliki() {
+        return sbnDimiliki;
+    }
+
+    public void beliSaham(String kode, int jumlah) {
+        sahamDimiliki.put(kode, sahamDimiliki.getOrDefault(kode, 0) + jumlah);
+    }
+
+    public void jualSaham(String kode, int jumlah) {
+        if (sahamDimiliki.containsKey(kode)) {
+            int sisa = sahamDimiliki.get(kode) - jumlah;
+            if (sisa <= 0) sahamDimiliki.remove(kode);
+            else sahamDimiliki.put(kode, sisa);
+        }
+    }
+
+    public void beliSBN(String nama, double nominal) {
+        sbnDimiliki.put(nama, sbnDimiliki.getOrDefault(nama, 0.0) + nominal);
+    }
 }
